96f4f7461019f7a3d89038975d94ddecad11efe3
ls29322 <erikbc_95@hotmail.com>
2018-03-15 18:16:01 +0100

    te piso te pisooo

--- app/src/main/java/com/androshopping/androshopping/MainActivity.java
+++ app/src/main/java/com/androshopping/androshopping/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        Log.d("msg","Prueba de merge");
+        Log.d("msg","Prueba de merge TEPISO NURIA TEPISOO");
         setContentView(R.layout.activity_main);
     }
 }
