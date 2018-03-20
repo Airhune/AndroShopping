a912f4913b9580baac9005e157fc048a52ae1d1f
Nuria Gómez Piedrafita <ls27346@salleurl.edu>
2018-03-20 11:57:31 +0100

    change activity action bar label

--- app/src/main/java/com/androshopping/androshopping/LoginActivity.java
+++ app/src/main/java/com/androshopping/androshopping/LoginActivity.java
@@ -3,8 +3,6 @@ package com.androshopping.androshopping;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
--- app/src/main/java/com/androshopping/androshopping/SplashActivity.java
+++ app/src/main/java/com/androshopping/androshopping/SplashActivity.java
@@ -11,7 +11,6 @@ public class SplashActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_splash);
 
         int delay = 2000;
