b1c919eb5fc52c269a97564249d563ae22a6a07f
Eric <erikbc_95@hotmail.com>
2018-03-26 22:40:12 +0200

    added login validation

--- app/src/main/java/com/androshopping/androshopping/LoginActivity.java
+++ app/src/main/java/com/androshopping/androshopping/LoginActivity.java
@@ -1,10 +1,15 @@
 package com.androshopping.androshopping;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import java.util.List;
 
@@ -19,8 +24,10 @@ public class LoginActivity extends Activity {
         validateButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent i = new Intent(getApplicationContext(), ListProductActivity.class);
-                startActivity(i);
+                if(loginIsCorrect()){
+                    Intent i = new Intent(getApplicationContext(), ListProductActivity.class);
+                    startActivity(i);
+                }
             }
         });
     }
@@ -30,5 +37,39 @@ public class LoginActivity extends Activity {
         moveTaskToBack(true);
     }
 
+    public boolean loginIsCorrect(){
 
+        EditText user_field = (EditText) findViewById(R.id.user_field);
+        String email = user_field.getText().toString();
+
+        EditText pw_field = (EditText) findViewById(R.id.pw_field);
+        String pw = pw_field.getText().toString();
+
+        if(emailIsCorrect(email)){
+            if(passwordIsCorrect(pw)){
+                return true;
+            }
+            else{
+                showToastMessage("Password needs 6 digits at least.");
+            }
+        }
+        else{
+            showToastMessage("Invalid email.");
+        }
+        return false;
+    }
+
+    public boolean emailIsCorrect(String email){
+        return (!TextUtils.isEmpty(email) && Patterns.EMAIL_ADDRESS.matcher(email).matches());
+    }
+
+    public boolean passwordIsCorrect(String pw){
+        return (pw.length() >= 6);
+    }
+
+    public void showToastMessage(String message){
+        Context context = getApplicationContext();
+        Toast toast = Toast.makeText(context, message, Toast.LENGTH_SHORT);
+        toast.show();
+    }
 }
